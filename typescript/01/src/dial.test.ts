7e82b3a47713efa5ada6a3728321adf51b1a487c
Joe Sak <joe@josak.com>
2025-12-04 14:41:25 -0700

    Wrap the dial

--- typescript/01/src/dial.test.ts
+++ typescript/01/src/dial.test.ts
@@ -7,7 +7,21 @@ describe('Dial', () => {
 
   test("turn the dial", () => {
     dial = new Dial();
+
     dial.turn("L", 5);
     expect(dial.position).toBe(45);
+
+    dial.turn("R", 10);
+    expect(dial.position).toBe(55);
+  });
+
+  test("turn the dial past 0/99", () => {
+    dial = new Dial();
+
+    dial.turn("L", 51);
+    expect(dial.position).toBe(99);
+
+    dial.turn("R", 2);
+    expect(dial.position).toBe(1);
   });
 })

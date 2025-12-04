b844c07c5b9cb7f6c8331849f06905a101969d28
Joe Sak <joe@josak.com>
2025-12-04 14:21:14 -0700

    add Dial.turn()

--- typescript/01/src/dial.test.ts
+++ typescript/01/src/dial.test.ts
@@ -4,4 +4,10 @@ describe('Dial', () => {
   test('initial conditions', () => {
     expect(new Dial().position).toBe(50)
   });
+
+  test("turn the dial", () => {
+    dial = new Dial();
+    dial.turn("L", 5);
+    expect(dial.position).toBe(45);
+  });
 })
--- typescript/01/src/dial.ts
+++ typescript/01/src/dial.ts
@@ -1,3 +1,8 @@
 export default class Dial {
   position = 50;
+
+  turn(dir: string, count: number) {
+    const delta = (dir === "L") ? -count : count;
+    this.position += delta;
+  }
 }

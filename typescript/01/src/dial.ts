64446a6cfbcae40e83560fe3ab8d43e4b08b456c
Joe Sak <joe@joesak.com>
2025-12-05 01:28:12 -0700

    Update dial.ts

--- typescript/01/src/dial.ts
+++ typescript/01/src/dial.ts
@@ -6,15 +6,13 @@ export default class Dial {
   turn(rotation: string, clicks: number) {
     const delta = rotation === "L" ? -1 : 1;
 
-    while(true) {
+    while(clicks > 0) {
       this.position += delta;
       if (this.position === -1) this.position = 99;
       if (this.position === 100) this.position = 0;
+      if (clicks > 0 && this.position === 0) this.zeroPasses++;
 
       clicks--;
-      if (clicks === 0) break;
-
-      if (this.position === 0) this.zeroPasses++;
     };
 
     if (this.position === 0) this.zeroLandings++;

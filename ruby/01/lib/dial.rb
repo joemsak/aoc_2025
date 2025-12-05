21552c4564b0b12a504b31442efcbac2c1743e6f
Joe Sak <joe@joesak.com>
2025-12-05 01:29:44 -0700

    Update dial.rb

--- ruby/01/lib/dial.rb
+++ ruby/01/lib/dial.rb
@@ -10,15 +10,14 @@ class Dial
   def turn(rotation, clicks)
     delta = rotation == "L" ? -1 : 1
 
-    loop do
+    while clicks > 0 do
       @position = position + delta
       @position = 99 if position === -1
       @position = 0 if position == 100
 
-      clicks -= 1
-      break if clicks.zero?
+      @zero_passes += 1 if clicks > 0 && position.zero?
 
-      @zero_passes += 1 if position.zero?
+      clicks -= 1
     end
 
     @zero_landings += 1 if position.zero?

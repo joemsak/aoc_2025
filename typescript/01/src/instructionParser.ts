c0203ddb27cd93269a8b5674dcf0846bb3d2fccd
Joe Sak <joe@josak.com>
2025-12-04 23:55:07 -0700

    Derp

--- typescript/01/src/instructionParser.ts
+++ typescript/01/src/instructionParser.ts
@@ -1,17 +1,17 @@
 export class Instruction {
-  public dir: string;
+  public rotation: string;
 	public count: number;
 
-  constructor(dir: string, count: string) {
-    this.dir = dir;
+  constructor(rotation: string, count: string) {
+    this.rotation = rotation.toUpper();
     this.count = parseInt(count);
   }
 }
 
 export function parseInstructions(list: string): Instruction[] {
   return list.split(/[\n,]\s*/).map(entry => {
-    const dir = entry.split("")[0];
+    const rotation = entry.split("")[0];
     const count = entry.substring(1);
-    return new Instruction(dir, count);
+    return new Instruction(rotation, count);
   });
 }

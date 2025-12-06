4098d52d793c61c64d8fe9bdf495df51598a41d6
Joe Sak <joe@josak.com>
2025-12-05 21:07:46 -0700

    Refactor responsibilities: Solver gives meaning to 0, dial is just a state machine

--- ruby/01/lib/dial.rb
+++ ruby/01/lib/dial.rb
@@ -1,25 +1,14 @@
 class Dial
-  attr_reader :position, :zero_landings, :zero_passes
+  attr_reader :position
 
   def initialize
     @position = 50
-    @zero_landings = 0
-    @zero_passes = 0
   end
 
-  def turn(rotation, clicks)
+  def step(rotation)
     delta = rotation == "L" ? -1 : 1
-
-    while clicks > 0 do
-      @position = position + delta
-      @position = 99 if position === -1
-      @position = 0 if position == 100
-
-      clicks -= 1
-
-      @zero_passes += 1 if clicks > 0 && position.zero?
-    end
-
-    @zero_landings += 1 if position.zero?
+    @position = position + delta
+    @position = 99 if position === -1
+    @position = 0 if position == 100
   end
 end
--- ruby/01/spec/lib/dial_spec.rb
+++ ruby/01/spec/lib/dial_spec.rb
@@ -12,73 +12,20 @@ RSpec.describe Dial do
   specify "turn the dial" do
     dial = described_class.new
 
-    dial.turn("L", 5)
-    expect(dial.position).to eq(45)
+    dial.step("L")
+    expect(dial.position).to eq(49)
 
-    dial.turn("R", 10)
-    expect(dial.position).to eq(55)
+    dial.step("R")
+    expect(dial.position).to eq(50)
   end
 
   specify "wrap around 0/99" do
     dial = described_class.new
 
-    dial.turn("L", 55)
-    expect(dial.position).to eq(95)
-
-    dial.turn("R", 5)
-    expect(dial.position).to eq(0)
-
-    dial.turn("L", 1)
+    51.times { dial.step("L") }
     expect(dial.position).to eq(99)
-  end
 
-  specify "keep count of landing on 0" do
-    dial = described_class.new
-
-    dial.turn("L", 50) # 0
-    expect(dial.zero_landings).to eq(1)
-
-    dial.turn("R", 1) # 1
-    dial.turn("L", 2) # 99
-    dial.turn("R", 1) # 0
-    expect(dial.zero_landings).to eq(2)
-  end
-
-  specify "keep count of passing 0" do
-    dial = described_class.new
-
-    dial.turn("L", 51) # 99
-    expect(dial.zero_passes).to eq(1)
-
-    dial.turn("R", 1) # 0
-    dial.turn("R", 1) # 1
-    dial.turn("L", 2) # 99
-    expect(dial.zero_passes).to eq(2)
-
-    dial.turn("L", 200) # 99 twice
-    expect(dial.zero_passes).to eq(4)
-
-    dial.turn("R", 1_000) # 99 ten times
-    expect(dial.zero_passes).to eq(14)
-  end
-
-  specify "turning after landing on 0 doesn't double count as a pass" do
-    dial = described_class.new
-
-    dial.turn("L", 50) # 0
-    dial.turn("R", 1) # 1
-    expect(dial.zero_landings).to eq(1)
-    expect(dial.zero_passes).to eq(0)
-
-    dial.turn("L", 2) # 99
-    expect(dial.zero_landings).to eq(1)
-    expect(dial.zero_passes).to eq(1)
-  end
-
-  specify "edge case: high number" do
-    dial = described_class.new
-
-    dial.turn("L", 876)
-    expect(dial.position).to eq(74)
+    dial.step("R")
+    expect(dial.position).to eq(0)
   end
 end

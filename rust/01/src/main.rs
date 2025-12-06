223c2f28b118e95b28b239dad8657c039a550092
Joe Sak <joe@josak.com>
2025-12-05 21:17:54 -0700

    Refactor rust dial so that it is a dumb state machine

--- rust/01/src/main.rs
+++ rust/01/src/main.rs
@@ -7,44 +7,43 @@ struct Instruction {
 
 struct Dial {
     position: i32,
-    zero_landings: u32,
-    zero_passes: u32,
 }
 
 impl Dial {
-    fn turn(&mut self, instruction: Instruction) {
-        let mut clicks = instruction.clicks;
+    fn step(&mut self, rotation: &String) {
+        let delta: i32 = if rotation == "L" { -1 } else { 1 };
 
-        while clicks > 0 {
-            let delta: i32 = if instruction.rotation == "L" { -1 } else { 1 };
+        self.position += delta;
 
-            self.position += delta;
-
-            if self.position == 100 {
-                self.position = 0;
-            } else if self.position == -1 {
-                self.position = 99;
-            }
-
-            clicks -= 1;
-
-            if clicks > 0 && self.position == 0 { self.zero_passes += 1 }
+        if self.position == 100 {
+            self.position = 0;
+        } else if self.position == -1 {
+            self.position = 99;
         }
-
-        if self.position == 0 { self.zero_landings += 1 }
     }
 }
 
 fn main() -> std::io::Result<()> {
     let input = fs::read_to_string("data/input.txt")?;
     let instructions = parse_instructions(&input);
-    let mut dial = Dial { position: 50, zero_landings: 0, zero_passes: 0 };
+    let mut dial = Dial { position: 50 };
+
+    let mut zero_landings: u32 = 0;
+    let mut zero_passes: u32 = 0;
 
     for instruction in instructions {
-        dial.turn(instruction);
+        let mut clicks = instruction.clicks;
+
+        while clicks > 0 {
+            dial.step(&instruction.rotation);
+            clicks -= 1;
+            if clicks > 0 && dial.position == 0 { zero_passes += 1 }
+        }
+
+        if dial.position == 0 { zero_landings += 1 }
     }
 
-    println!("Password: {}", dial.zero_landings + dial.zero_passes);
+    println!("Password: {}", zero_landings + zero_passes);
     Ok(())
 }
 

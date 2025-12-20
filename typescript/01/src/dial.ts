6cc44743a066f1b66b0c29a6df16c5d054d7d9f1
Joe Sak <joe@joesak.com>
2025-12-19 22:48:23 -0700

    Wrap the dial

--- typescript/01/src/dial.ts
+++ typescript/01/src/dial.ts
@@ -1,8 +1,18 @@
 export default class Dial {
+  private static readonly LIMIT = 100;
+
   position = 50;
 
   turn(dir: string, count: number) {
-    const delta = (dir === "L") ? -count : count;
-    this.position += delta;
+    const limitedCount = count % Dial.LIMIT;
+    const delta = (dir === "L") ? -limitedCount : limitedCount;
+
+    // remainder -> modulo in JS:
+    //   - (n % d) + d) % d
+    const n = this.position + delta; // dividend
+    const d = Dial.LIMIT;            // divisor
+    const result = ((n % d) + d) % d
+
+    this.position = result;
   }
 }

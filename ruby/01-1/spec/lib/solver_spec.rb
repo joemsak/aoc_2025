b6248aaad79b72cbc79d0e7a679f76068ada7704
Joe Sak <joe@joesak.com>
2025-12-19 22:48:23 -0700

    Update solver_spec.rb

--- ruby/01-1/spec/lib/solver_spec.rb
+++ ruby/01-1/spec/lib/solver_spec.rb
@@ -3,10 +3,10 @@ require "solver"
 
 RSpec.describe Solver do
   it "returns the correct count when dial points at zero" do
-    solver = Solver.new("L50")
+    solver = described_class.new("L50")
     expect(solver.solve).to eq(1)
 
-    solver = Solver.new("L50\nR1\nL1")
+    solver = described_class.new("L50\nR1\nL1")
     expect(solver.solve).to eq(2)
   end
 end

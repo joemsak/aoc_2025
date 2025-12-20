c85defb191af1b6b57f3478e9f64a68f8dd100ff
Joe Sak <joe@joesak.com>
2025-12-19 22:48:23 -0700

    Refactor responsibilities: Solver gives meaning to 0, dial is just a state machine

--- ruby/01/lib/solver.rb
+++ ruby/01/lib/solver.rb
@@ -2,16 +2,34 @@ require_relative "./instruction_parser"
 require_relative "./dial"
 
 class Solver
+  attr_reader :instructions
+
   def initialize(list)
     @instructions = InstructionParser.new(list).instructions
     @dial = Dial.new
+    @zero_passes = 0
+    @zero_landings = 0
   end
 
   def solve
-    @instructions.each do |instruction|
-      @dial.turn(instruction.rotation, instruction.clicks)
+    instructions.each do |instruction|
+      turn_dial(instruction.rotation, instruction.clicks)
     end
 
-    @dial.zero_passes + @dial.zero_landings
+    zero_passes + zero_landings
+  end
+
+  private
+
+  attr_reader :dial, :zero_passes, :zero_landings
+
+  def turn_dial(rotation, clicks)
+    while clicks > 0 do
+      dial.step(rotation)
+      clicks -= 1
+      @zero_passes += 1 if clicks > 0 && dial.position.zero?
+    end
+
+    @zero_landings += 1 if dial.position.zero?
   end
 end
--- ruby/01/spec/integration_spec.rb
+++ ruby/01/spec/integration_spec.rb
@@ -7,10 +7,13 @@ require "instruction_parser"
 RSpec.describe "integration" do
   specify do
     dial = Dial.new
-    instructions = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82"
+    input = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82"
+    parser = InstructionParser.new(input)
 
-    InstructionParser.new(instructions).instructions.each do |instruction|
-      dial.turn(instruction.rotation, instruction.clicks)
+    parser.instructions.each do |instruction|
+      instruction.clicks.times do
+        dial.step(instruction.rotation)
+      end
     end
 
     expect(dial.position).to eq(32)

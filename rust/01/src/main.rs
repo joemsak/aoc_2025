7ce71bc8867b6660273036d7bb7d868f507c61f7
Joe Sak <joe@joesak.com>
2025-12-19 22:48:23 -0700

    Finish Day 1-2 for Rust

--- rust/01/src/main.rs
+++ rust/01/src/main.rs
@@ -1,15 +1,51 @@
+use std::fs;
+
 struct Instruction {
     rotation: String,
     clicks: u32,
 }
 
-fn main() {
-    let input = "L32\nR1";
-    let instructions = parse_instructions(input);
+struct Dial {
+    position: i32,
+    zero_landings: u32,
+    zero_passes: u32,
+}
+
+impl Dial {
+    fn turn(&mut self, instruction: Instruction) {
+        let mut clicks = instruction.clicks;
+
+        while clicks > 0 {
+            let delta: i32 = if instruction.rotation == "L" { -1 } else { 1 };
+
+            self.position += delta;
+
+            if self.position == 100 {
+                self.position = 0;
+            } else if self.position == -1 {
+                self.position = 99;
+            }
+
+            clicks -= 1;
+
+            if clicks > 0 && self.position == 0 { self.zero_passes += 1 }
+        }
+
+        if self.position == 0 { self.zero_landings += 1 }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let input = fs::read_to_string("data/input.txt")?;
+    let instructions = parse_instructions(&input);
+    let mut dial = Dial { position: 50, zero_landings: 0, zero_passes: 0 };
 
     for instruction in instructions {
-        println!("rotation: {}, clicks: {}", instruction.rotation, instruction.clicks);
+        dial.turn(instruction);
     }
+
+    println!("Password: {}", dial.zero_landings + dial.zero_passes);
+    Ok(())
 }
 
 fn parse_instructions(list: &str) -> Vec<Instruction> {
